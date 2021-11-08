ba902d3283b7d2464c4f4d37e795ef86797fc44e
walter-wang314 <wzc314159@hotmail.com>
2021-11-09 00:55:36 +0800

    update storybook

---
+++ src/stories/Button.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Button from '../components/Button/button';
+
+export default {
+  title: 'Button',
+  component: Button,
+};
+
+export const DefaultButton = () => <Button btnType="primary">Default</Button>;
